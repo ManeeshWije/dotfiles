19e07374bb0e9133ab72b7565e11b8617d020a02
ManeeshWije <m.mwije1@gmail.com>
2022-05-31 20:30:30 -0400

    live grep works with telescope now

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,6 @@ require('packer').startup(function()
   use 'arcticicestudio/nord-vim' -- colorscheme
   use 'epilande/vim-react-snippets' -- react snippets
   use 'nvim-lua/popup.nvim' -- popup
-  use 'BurntSushi/ripgrep' -- ripgrep
 end)
 
 require'nvim-treesitter.configs'.setup {
@@ -248,6 +247,7 @@ nnoremap <leader>n :NvimTreeFindFile<CR>
 
 nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>fs <cmd>lua require('telescope.builtin').grep_string()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
 ]])
