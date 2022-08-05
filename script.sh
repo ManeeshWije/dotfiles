ba01ac03a670b52b6888a3b2135d97e206be6cb7
ManeeshWije <m.mwije1@gmail.com>
2022-08-04 21:04:30 -0400

    added postman

--- script.sh
+++ script.sh
@@ -5,14 +5,12 @@
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/maneeshwijewardhana/.profile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 # install everything
-brew install via ninja rust-analyzer ripgrep fzf ccls clang-format cmake cmatrix git go heroku htop llvm lua mongosh ncdu neofetch neovim node nvm pfetch python speedtest-cli starship tmux tty-clock audacity balenaetcher brave-browser firefox caffeine discord microsoft-teams minecraft slack spotify visual-studio-code zoom alacritty
+brew install via postman eloston-chromium ninja rust-analyzer ripgrep fzf ccls clang-format cmake cmatrix git go heroku htop llvm lua mongosh ncdu neofetch neovim node nvm pfetch python speedtest-cli starship tmux tty-clock audacity balenaetcher firefox caffeine discord microsoft-teams minecraft slack spotify visual-studio-code zoom alacritty
 
 ssh-keygen -t ed25519 -C "m.mwije1@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add -K ~/.ssh/id_ed25519
 
 # clone projects
-# cd Documents
-# mkdir Projects
-# cd Projects
+# mkdir ~/dev/Projects && cd dev/Projects
 # git clone git@github.com:ManeeshWije/Personal-Website.git && git clone git@github.com:ManeeshWije/Tasker.git && git clone git@github.com:ManeeshWije/GoChat.git && git clone git@github.com:ManeeshWije/pokedex.git && git clone git@github.com:ManeeshWije/abby.git && git clone git@github.com:ManeeshWije/application-tracker.git && git clone git@github.com:ManeeshWije/Reduction.git && git clone git@github.com:ManeeshWije/GetBack2Wrk.git && git clone git@github.com:ManeeshWije/twitterLikeBot.git && git clone git@github.com:ManeeshWije/Baby-Talk.git
