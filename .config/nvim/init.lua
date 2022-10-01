f6020ede74902668876690ff9427471f4fb04099
ManeeshWije <m.mwije1@gmail.com>
2022-10-01 14:36:48 -0400

    change cmd height to match tmux bar

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -190,7 +190,6 @@ end
 local options = {
   backup = false, -- creates a backup file
   clipboard = 'unnamedplus', -- allows neovim to access the system clipboard
-  cmdheight = 2, -- more space in the neovim command line for displaying messages
   completeopt = { 'menuone', 'noselect' }, -- mostly just for cmp
   fileencoding = 'utf-8', -- the encoding written to a file
   hlsearch = true, -- highlight all matches on previous search pattern
