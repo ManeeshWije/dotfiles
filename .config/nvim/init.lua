720f85fd8c98ab98d6afec7fdfc984b8d3d71dda
ManeeshWije <m.mwije1@gmail.com>
2022-10-01 14:31:41 -0400

    bracket pair colors and tmux theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,6 +38,7 @@ require('packer').startup(function()
   use 'mlaursen/vim-react-snippets' -- react snippets
   use 'nvim-lua/popup.nvim' -- popup
   use { 'turbio/bracey.vim', run = 'npm install --prefix server' } -- live server
+  use 'p00f/nvim-ts-rainbow' -- bracket pair colourizer
 end)
 
 require('nvim-treesitter.configs').setup({
@@ -48,7 +49,15 @@ require('nvim-treesitter.configs').setup({
   },
   indent = {
     enable = true
-  }
+  },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    colors = { '#BF616A', '#D08770', '#EBCB8B', '#5E81AC', '#B48EAD' }, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
 })
 require('lualine').setup({
   options = { theme = 'nord' }
--- setup.sh
+++ setup.sh
@@ -16,6 +16,5 @@ git config status.showuntrackedfiles no
 # some lsp
 npm install -g pyright bash-language-server dockerfile-language-server-nodejs vscode-langservers-extracted emmet-ls sql-language-server vim-language-server
 
-# clone projects
-# mkdir ~/Documents/Projects && cd Documents/Projects
-# git clone git@github.com:ManeeshWije/Personal-Website.git && git clone git@github.com:ManeeshWije/Tasker.git && git clone git@github.com:ManeeshWije/GoChat.git && git clone git@github.com:ManeeshWije/pokedex.git && git clone git@github.com:ManeeshWije/abby.git && git clone git@github.com:ManeeshWije/application-tracker.git && git clone git@github.com:ManeeshWije/Reduction.git && git clone git@github.com:ManeeshWije/GetBack2Wrk.git && git clone git@github.com:ManeeshWije/twitterLikeBot.git && git clone git@github.com:ManeeshWije/Baby-Talk.git
+# clone stuff
+git clone https://github.com/ManeeshWije/dotfiles.git
