b3ff78a4bb2af6b8df737475bbcd1ebc4551597d
ManeeshWije <m.mwije1@gmail.com>
2023-10-15 03:31:06 -0400

    .

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install everything
 brew tap homebrew/cask-fonts
-brew install tree font-iosevka-nerd-font mactex pandoc qlmarkdown maccy gh black flake8 google-java-format stylua ninja tldr ltex-ls yabai skhd jdtls java lua-language-server bear via postman rust-analyzer ripgrep clang-format cmake git go htop llvm lua mongosh ncdu neofetch neovim node nvm pfetch python speedtest-cli starship tmux audacity balenaetcher caffeine discord microsoft-teams minecraft slack spotify visual-studio-code zoom alacritty mvn firefox 
+brew install tree font-iosevka-nerd-font mactex pandoc qlmarkdown maccy gh black flake8 google-java-format stylua ninja tldr ltex-ls yabai skhd jdtls java lua-language-server bear via postman rust-analyzer ripgrep clang-format cmake git go htop llvm lua mongosh ncdu neofetch neovim node nvm pfetch python speedtest-cli tmux audacity balenaetcher caffeine discord microsoft-teams minecraft slack spotify visual-studio-code zoom alacritty mvn firefox
 brew install --cask MonitorControl flux
 
 ssh-keygen -t ed25519 -C "m.mwije1@gmail.com"
