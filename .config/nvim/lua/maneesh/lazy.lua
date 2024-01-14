572eb079f656a3e68efd4f8251cb72124a472f58
ManeeshWije <m.mwije1@gmail.com>
2024-01-14 14:48:51 -0500

    telescope undo

--- .config/nvim/lua/maneesh/lazy.lua
+++ .config/nvim/lua/maneesh/lazy.lua
@@ -38,9 +38,19 @@ local plugins = {
     { 'rafamadriz/friendly-snippets' },
 
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "debugloop/telescope-undo.nvim",
+        },
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    undo = {},
+                },
+            })
+            require("telescope").load_extension("undo")
+        end,
     },
 
     {
