241c048f777b382843ae1ebe4af0cfa990718a24
ManeeshWije <m.mwije1@gmail.com>
2024-03-06 16:25:32 -0500

    keymap undo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -456,7 +456,7 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 vim.keymap.set("n", "<leader>fe", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
-vim.keymap.set("n", "<leader>ud", "<cmd>Telescope undo<cr>")
+vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>")
 
 vim.keymap.set("n", "<leader>p", function()
     require("conform").format()
