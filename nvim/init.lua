03b59d87f937772d96120ad292309dbe804a2e71
ManeeshWije <m.mwije1@gmail.com>
2024-03-09 21:40:25 -0500

    annoying

--- nvim/init.lua
+++ nvim/init.lua
@@ -268,7 +268,6 @@ require("lazy").setup({
                         luasnip.lsp_expand(args.body)
                     end,
                 },
-                completion = { completeopt = "menu,menuone,noinsert" },
                 mapping = cmp.mapping.preset.insert({
                     ["<C-n>"] = cmp.mapping.select_next_item(),
                     ["<C-p>"] = cmp.mapping.select_prev_item(),
