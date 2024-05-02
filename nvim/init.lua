81f926df9769c34578d47551b49e7f36beda26f6
ManeeshWije <m.mwije1@gmail.com>
2024-05-02 18:50:31 -0400

    update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 vim.opt.hlsearch = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -100,13 +101,48 @@ require("lazy").setup({
             pcall(require("telescope").load_extension, "ui-select")
 
             local builtin = require("telescope.builtin")
+            local action_state = require("telescope.actions.state")
+            local actions = require("telescope.actions")
+
+            local buffer_searcher
+            buffer_searcher = function()
+                builtin.buffers({
+                    sort_mru = true,
+                    ignore_current_buffer = false,
+                    show_all_buffers = false,
+                    attach_mappings = function(prompt_bufnr, map)
+                        local refresh_buffer_searcher = function()
+                            actions.close(prompt_bufnr)
+                            vim.schedule(buffer_searcher)
+                        end
+                        local delete_buf = function()
+                            local selection = action_state.get_selected_entry()
+                            vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+                            refresh_buffer_searcher()
+                        end
+                        local delete_multiple_buf = function()
+                            local picker = action_state.get_current_picker(prompt_bufnr)
+                            local selection = picker:get_multi_selection()
+                            for _, entry in ipairs(selection) do
+                                vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+                            end
+                            refresh_buffer_searcher()
+                        end
+                        map("n", "dd", delete_buf)
+                        map("n", "<C-d>", delete_multiple_buf)
+                        map("i", "<C-d>", delete_multiple_buf)
+                        return true
+                    end,
+                })
+            end
+
             vim.keymap.set("n", "<leader>fh", builtin.help_tags)
             vim.keymap.set("n", "<leader>fk", builtin.keymaps)
             vim.keymap.set("n", "<leader>ff", builtin.find_files)
             vim.keymap.set("n", "<leader>fw", builtin.grep_string)
             vim.keymap.set("n", "<leader>fg", builtin.live_grep)
             vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
-            vim.keymap.set("n", "<leader>fb", builtin.buffers)
+            vim.keymap.set("n", "<leader>fb", buffer_searcher, {})
         end,
     },
 
@@ -223,10 +259,10 @@ require("lazy").setup({
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "isort", "black" },
-                javascript = { "prettierd" },
-                typescript = { "prettierd" },
-                javascriptreact = { "prettierd" },
-                typescriptreact = { "prettierd" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
                 java = { "google-java-format" },
                 go = { "gofmt", "goimports" },
                 rust = { "rustfmt" },
@@ -309,18 +345,6 @@ require("lazy").setup({
             vim.cmd([[let g:gruvbox_material_diagnostic_virtual_text = "colored"]])
             vim.cmd([[let g:gruvbox_material_transparent_background = 1]])
             vim.cmd.colorscheme("gruvbox-material")
-        end,
-    },
-
-    { -- QOL improvements
-        "echasnovski/mini.nvim",
-        config = function()
-            require("mini.ai").setup({ n_lines = 500 })
-            require("mini.surround").setup()
-            require("mini.statusline").setup()
-            MiniStatusline.section_location = function()
-                return "%2l:%-2v"
-            end
         end,
     },
 
