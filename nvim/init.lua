08da9b12681430ddfa96fa22664b24a73af76f39
Maneesh Wije <m.mwije1@proton.me>
2024-12-24 20:48:19 -0500

    fix(nvim): set showmode

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,6 @@ vim.opt.guicursor = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
-vim.opt.showmode = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
 vim.opt.undofile = true
