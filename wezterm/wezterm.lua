f7ada6146e719937fa667430c6650ba59cd885aa
maneesh <m.mwije1@proton.me>
2025-08-06 11:33:28 -0400

    use vim for window navigation, dont need these binds anymore

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -38,41 +38,14 @@ config.window_frame = {
     inactive_titlebar_bg = "#000000",
 }
 
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
 config.keys = {
     { key = "l",         mods = "SUPER",        action = wezterm.action.ShowLauncher },
-
-    { key = "-",         mods = "LEADER",       action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { key = "\\",        mods = "LEADER",       action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { key = "h",         mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
-    { key = "j",         mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
-    { key = "k",         mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
-    { key = "l",         mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
-    { key = "x",         mods = "LEADER",       action = act.CloseCurrentPane({ confirm = true }) },
-    { key = "z",         mods = "LEADER",       action = act.TogglePaneZoomState },
-    { key = "o",         mods = "LEADER",       action = act.RotatePanes("Clockwise") },
-
-    { key = "c",         mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
-    { key = "w",         mods = "LEADER",       action = act.CloseCurrentTab({ confirm = true }) },
-    { key = "p",         mods = "LEADER",       action = act.ActivateTabRelative(-1) },
-    { key = "n",         mods = "LEADER",       action = act.ActivateTabRelative(1) },
-
-    { key = "1",         mods = "LEADER",       action = act.ActivateTab(0) },
-    { key = "2",         mods = "LEADER",       action = act.ActivateTab(1) },
-    { key = "3",         mods = "LEADER",       action = act.ActivateTab(2) },
-    { key = "4",         mods = "LEADER",       action = act.ActivateTab(3) },
-    { key = "5",         mods = "LEADER",       action = act.ActivateTab(4) },
-    { key = "6",         mods = "LEADER",       action = act.ActivateTab(5) },
-    { key = "7",         mods = "LEADER",       action = act.ActivateTab(6) },
-    { key = "8",         mods = "LEADER",       action = act.ActivateTab(7) },
-    { key = "9",         mods = "LEADER",       action = act.ActivateTab(-1) },
-
-
-    { key = "[",         mods = "LEADER",       action = act.ActivateCopyMode },
+    { key = "[",         mods = "SUPER",        action = act.ActivateCopyMode },
     { key = "UpArrow",   mods = "SHIFT",        action = act.ScrollByLine(-1) },
     { key = "DownArrow", mods = "SHIFT",        action = act.ScrollByLine(1) },
     { key = "PageUp",    mods = "NONE",         action = act.ScrollByPage(-1) },
     { key = "PageDown",  mods = "NONE",         action = act.ScrollByPage(1) },
+    { key = "s",         mods = "SUPER",        action = sessionizer.switch_workspace() }
 }
 
 sessionizer.set_projects(projects)
