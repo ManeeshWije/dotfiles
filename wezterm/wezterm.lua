f5c80113ea25b182b07936982cf2b90a2149fec0
maneesh <m.mwije1@proton.me>
2025-08-12 16:27:37 -0400

    need resize

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -29,7 +29,7 @@ config.window_padding = {
     top = 4,
     bottom = 4,
 }
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
 config.window_frame = {
     font = wezterm.font({ family = "IosevkaTerm Nerd Font", weight = "Bold" }),
