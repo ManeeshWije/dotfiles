4e733f9a3134cda696d3217df34a0aa8cebe7251
maneesh <m.mwije1@proton.me>
2025-10-09 12:20:43 -0400

    completion setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,7 +54,7 @@ vim.pack.add({
     { src = "https://github.com/neovim/nvim-lspconfig" }
 })
 vim.cmd(":hi statusline guibg=NONE")
-vim.cmd("set completeopt+=noselect")
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
 
 local all_levels = {
     vim.diagnostic.severity.ERROR,
@@ -315,7 +315,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+            vim.lsp.completion.enable(true, client.id, ev.buf, {
+                autotrigger = true,
+                convert = function(item)
+                    return { abbr = item.label:gsub("%b()", "") }
+                end,
+            })
         end
     end,
 })
