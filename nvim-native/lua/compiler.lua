d37a56b967f5489af4be208a12762de8ecb85d19
ManeeshWije <m.mwije1@proton.me>
2026-08-12 10:25:33 -0400

    need to still set compiler

--- nvim-native/lua/compiler.lua
+++ nvim-native/lua/compiler.lua
@@ -1,21 +1,21 @@
--- vim.g.tsc_makeprg = "npx tsc"
+vim.g.tsc_makeprg = "npx tsc"
 --
--- local compiler_configs = {
--- 	typescript = {
--- 		patterns = { "typescript", "typescriptreact" },
--- 		compiler = "tsc",
--- 	},
--- }
---
--- for _, config in pairs(compiler_configs) do
--- 	vim.api.nvim_create_autocmd("FileType", {
--- 		pattern = config.patterns,
--- 		callback = function()
--- 			vim.cmd("compiler " .. config.compiler)
--- 		end,
--- 		desc = "Set up compiler for " .. table.concat(config.patterns, ", "),
--- 	})
--- end
+local compiler_configs = {
+	typescript = {
+		patterns = { "typescript", "typescriptreact" },
+		compiler = "tsc",
+	},
+}
+
+for _, config in pairs(compiler_configs) do
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = config.patterns,
+		callback = function()
+			vim.cmd("compiler " .. config.compiler)
+		end,
+		desc = "Set up compiler for " .. table.concat(config.patterns, ", "),
+	})
+end
 
 -- open qflist after make to go through errors
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
