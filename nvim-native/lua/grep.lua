349341939fb757eda85d7c8d31c00e236d00eaa3
ManeeshWije <m.mwije1@proton.me>
2026-08-17 14:03:33 -0400

    og was better

--- nvim-native/lua/grep.lua
+++ nvim-native/lua/grep.lua
@@ -1,42 +1,11 @@
+vim.opt.grepprg = "rg --vimgrep --smart-case --hidden"
 vim.opt.grepformat = "%f:%l:%c:%m"
 
-local function grep_complete(arglead, _, _)
-	if arglead == "" then
-		return {}
-	end
-
-	local lines = vim.fn.systemlist({
-		"rg",
-		"--no-heading",
-		"--no-filename",
-		"--color=never",
-		"--smart-case",
-		"--hidden",
-		"--glob",
-		"!.git",
-		"--",
-		arglead,
-	})
-
-	local words = {}
-
-	for _, line in ipairs(lines) do
-		for word in line:gmatch("[%w_%.%-]+") do
-			if word:lower():find(arglead:lower(), 1, true) then
-				words[word] = true
-			end
+vim.keymap.set("n", "<leader>sg", function()
+	vim.ui.input({ prompt = "Grep: " }, function(pattern)
+		if pattern then
+			vim.cmd("silent grep! " .. vim.fn.fnameescape(pattern))
+			vim.cmd("copen")
 		end
-	end
-
-	return vim.tbl_keys(words)
-end
-
-vim.api.nvim_create_user_command("Grep", function(opts)
-	vim.cmd("silent grep! " .. vim.fn.shellescape(opts.args))
-	vim.cmd("copen")
-end, {
-	nargs = "+",
-	complete = grep_complete,
-})
-
-vim.keymap.set("n", "<leader>sg", ":Grep ", { silent = false })
+	end)
+end, { silent = true })
