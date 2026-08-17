ca8426d5e2d6aec91ba8cc52d1ca1e6fcf53b40d
ManeeshWije <m.mwije1@proton.me>
2026-08-17 13:57:14 -0400

    test grep

--- nvim-native/lua/grep.lua
+++ nvim-native/lua/grep.lua
@@ -1,11 +1,33 @@
 vim.opt.grepprg = "rg --vimgrep --smart-case --hidden"
 vim.opt.grepformat = "%f:%l:%c:%m"
 
-vim.keymap.set("n", "<leader>sg", function()
-	vim.ui.input({ prompt = "Grep: " }, function(pattern)
-		if pattern then
-			vim.cmd("silent grep! " .. vim.fn.fnameescape(pattern))
-			vim.cmd("copen")
-		end
-	end)
-end, { silent = true })
+local function grep_complete(arglead, cmdline, _)
+	local pattern = cmdline:match("^Grep%s+(.*)$") or arglead
+
+	if pattern == "" then
+		return {}
+	end
+
+	local results = vim.fn.systemlist({
+		"rg",
+		"--no-heading",
+		"--color=never",
+		"--smart-case",
+		"--hidden",
+		"--glob",
+		"!.git",
+		pattern,
+	})
+
+	return vim.list_slice(results, 1, 50)
+end
+
+vim.api.nvim_create_user_command("Grep", function(opts)
+	vim.cmd("silent grep! " .. vim.fn.shellescape(opts.args))
+	vim.cmd("copen")
+end, {
+	nargs = "+",
+	complete = grep_complete,
+})
+
+vim.keymap.set("n", "<leader>sg", ":Grep ", { silent = false })
