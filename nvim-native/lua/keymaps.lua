737f45f1ae9077c46d4285f44925820c5a62bca8
ManeeshWije <m.mwije1@proton.me>
2026-08-25 09:04:45 -0400

    dont change register on viwp

--- nvim-native/lua/keymaps.lua
+++ nvim-native/lua/keymaps.lua
@@ -21,6 +21,7 @@ vim.keymap.set("c", "<C-u>", "<PageUp>", { remap = false })
 vim.keymap.set("n", "<leader>fb", ":buffer ", { silent = false })
 vim.keymap.set("n", "<leader>vs", ":vs<CR>", { desc = "vsplit" })
 vim.keymap.set("n", "<C-x>", ":bd<CR>", { desc = "normal mode buffer delete" })
+vim.keymap.set("x", "p", "pgvy", { noremap = true, silent = true })
 -- close buffer in wildmenu
 vim.keymap.set("c", "<C-x>", function()
 	if vim.fn.getcmdtype() ~= ":" then
