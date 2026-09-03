bad16a9535bba09246a8e9c786436d27b5e22a61
ManeeshWije <m.mwije1@proton.me>
2026-09-03 10:14:51 -0400

    fix grep

--- nvim-native/lua/grep.lua
+++ nvim-native/lua/grep.lua
@@ -4,7 +4,7 @@ vim.opt.grepformat = "%f:%l:%c:%m"
 vim.keymap.set("n", "<leader>sg", function()
 	vim.ui.input({ prompt = "Grep: " }, function(pattern)
 		if pattern and pattern ~= "" then
-			vim.cmd.grep({ pattern, bang = true })
+			vim.cmd("grep! " .. vim.fn.shellescape(pattern))
 			vim.cmd.copen()
 		end
 	end)
